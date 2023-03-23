3f7ed611a138232a78d7acf3086bf28a0ea0f904
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-03-23 19:14:42 +0100

    create init template

--- index.php
+++ index.php
@@ -10,13 +10,47 @@ require_once __DIR__ . '/models/Toys.php';
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <!-- METATAGS -->
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <!-- TITLE -->
+  <title>BooleanPlanet</title>
+
+  <!-- BOOTSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+
+  <!-- FONT AWESOME -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!-- STYLE -->
+  <link rel="stylesheet" href="../frontend/CSS/style.css" />
+
+  <!-- AXIOS -->
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+  <!-- VUE JS -->
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+  <!-- TAILWIND -->
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  <!-- JS -->
+  <script type="text/javascript" src="../frontend/JS/app.js" defer></script>
 </head>
 
 <body>
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card">Hello</div>
+      </div>
+    </div>
+  </div>
 
 </body>
 
